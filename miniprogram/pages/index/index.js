43727ef8ddfee68c80c60a0bfea21c1504faa17a
kingc <1126663073@qq.com>
2019-05-24 22:59:18 +0800

    授权信息

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -44,7 +44,7 @@ const page = new Page({
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
-          // that.setData({ noauth:false});
+          that.setData({ noauth:false});
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
